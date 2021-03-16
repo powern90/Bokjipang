2f28248f76c2d6d4c7d9240b029636f82765a8db
Ryeoly <youddbb@naver.com>
2021-03-16 11:13:11 +0900

    21.03.16 item list(yoon deuk ryeol)

--- app/src/main/java/com/bluemango/bokjipang/fragment_home.java
+++ app/src/main/java/com/bluemango/bokjipang/fragment_home.java
@@ -2,6 +2,7 @@ package com.bluemango.bokjipang;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,11 +13,53 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class fragment_home extends Fragment {
+
+    private ViewPager viewPager;
+    private TextViewPagerAdapter pagerAdapter;
+
+    int currentPage = 0;
+    Timer timer;
+    final long DELAY_MS = 500;//delay in milliseconds before task is to be executed
+    final long PERIOD_MS = 3000; // time in milliseconds between successive task executions.
+
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        setHasOptionsMenu(true);
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        setHasOptionsMenu(true);                                    //상단바 옵션 생성
+
+        /** 텍스트뷰 자동 슬라이드 */
+        viewPager = view.findViewById(R.id.viewPager);
+        pagerAdapter = new TextViewPagerAdapter(getActivity());
+        viewPager.setAdapter(pagerAdapter);
+
+        final Handler handler = new Handler();
+        final Runnable Update = new Runnable() {
+            @Override
+            public void run() {
+                if(currentPage == 9) {
+                    currentPage = 0;
+                }
+                viewPager.setCurrentItem(currentPage++, true);
+            }
+        };
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(Update);
+            }
+        }, DELAY_MS, PERIOD_MS);
+        /**여기까지 자동 슬라이드 끝*/
+
+
+        return view;
     }
 
     @Override
