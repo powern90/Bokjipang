32e821fa5295ca2dcabc38f91a64a2e421dfd49c
xjwl0305 <xjwl0305@gmail.com>
2021-03-17 20:09:44 +0900

    2021.03.17 윤인석 회원가입2.1

--- app/src/main/java/com/bluemango/bokjipang/fragment_login.java
+++ app/src/main/java/com/bluemango/bokjipang/fragment_login.java
@@ -19,10 +19,10 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.kakao.auth.Session;
-import com.kakao.usermgmt.LoginButton;
-import com.kakao.usermgmt.UserManagement;
-import com.kakao.usermgmt.callback.LogoutResponseCallback;
+//import com.kakao.auth.Session;
+//import com.kakao.usermgmt.LoginButton;
+//import com.kakao.usermgmt.UserManagement;
+//import com.kakao.usermgmt.callback.LogoutResponseCallback;
 import org.w3c.dom.Text;
 import java.util.zip.Inflater;
 
@@ -32,9 +32,9 @@ public class fragment_login extends Fragment {
     private fragment_home fragment_home = new fragment_home();
     private fragment_signup fragment_signup = new fragment_signup();
 
-    Button kakaoLogin,kakaoLogout;
-    LoginButton loginButton;
-    private KaKaoCallBack kaKaoCallBack;
+//    Button kakaoLogin,kakaoLogout;
+//    LoginButton loginButton;
+//    private KaKaoCallBack kaKaoCallBack;
 
     public View onCreateView(LayoutInflater layoutInflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState) {
         View view = layoutInflater.inflate(R.layout.fragment_login, container, false);
@@ -78,58 +78,58 @@ public class fragment_login extends Fragment {
         });
 
 
-        /**KAKAO 로그인 */
-        loginButton = view.findViewById(R.id.loginButton);
-        kakaoLogin = view.findViewById(R.id.kakaoLogin);
-        kakaoLogout = view.findViewById(R.id.kakaoLogout);
-
-        kaKaoCallBack = new KaKaoCallBack();
-        Session.getCurrentSession().addCallback(kaKaoCallBack);
-        Session.getCurrentSession().checkAndImplicitOpen();
-
-        kakaoLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loginButton.performClick();
-                MainActivity activity = (MainActivity) getActivity();
-                activity.fm.beginTransaction().replace(R.id.fragment_container,fragment_signup).commit();
-            }
-        });
-
-        kakaoLogout.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                UserManagement.getInstance().requestLogout(new LogoutResponseCallback() {
-                    @Override
-                    public void onCompleteLogout() {
-                        Log.d("logout","되었음");
-                    }
-                });
-            }
-        });
-        /**KAKAO 로그인 */
+//        /**KAKAO 로그인 */
+//        loginButton = view.findViewById(R.id.loginButton);
+//        kakaoLogin = view.findViewById(R.id.kakaoLogin);
+//        kakaoLogout = view.findViewById(R.id.kakaoLogout);
+//
+//        kaKaoCallBack = new KaKaoCallBack();
+//        Session.getCurrentSession().addCallback(kaKaoCallBack);
+//        Session.getCurrentSession().checkAndImplicitOpen();
+//
+//        kakaoLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                loginButton.performClick();
+//                MainActivity activity = (MainActivity) getActivity();
+//                activity.fm.beginTransaction().replace(R.id.fragment_container,fragment_signup).commit();
+//            }
+//        });
+//
+//        kakaoLogout.setOnClickListener(new Button.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                UserManagement.getInstance().requestLogout(new LogoutResponseCallback() {
+//                    @Override
+//                    public void onCompleteLogout() {
+//                        Log.d("logout","되었음");
+//                    }
+//                });
+//            }
+//        });
+//        /**KAKAO 로그인 */
         return view;
     }
 
-    /**KAKAO 로그인 함수 */
-
-    public void kakaoError(String msg){
-        Toast.makeText(getActivity().getApplicationContext(),msg,Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Session.getCurrentSession().removeCallback(kaKaoCallBack);
-    }
-    /**KAKAO 로그인 함수 */
+//    /**KAKAO 로그인 함수 */
+//
+//    public void kakaoError(String msg){
+//        Toast.makeText(getActivity().getApplicationContext(),msg,Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if(Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+//            super.onActivityResult(requestCode, resultCode, data);
+//        }
+//    }
+//
+//    @Override
+//    public void onDestroy() {
+//        super.onDestroy();
+//        Session.getCurrentSession().removeCallback(kaKaoCallBack);
+//    }
+//    /**KAKAO 로그인 함수 */
 }
 
