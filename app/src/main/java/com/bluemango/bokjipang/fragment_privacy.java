474cbd9bb585b1f512232dcc5a0254abeb7dd29a
최재혁 <cjaeh9770@naver.com>
2021-03-25 18:29:29 +0900

    21.03.25 설정 쪽이랑, 로그인 api

---
+++ app/src/main/java/com/bluemango/bokjipang/fragment_privacy.java
@@ -0,0 +1,16 @@
+package com.bluemango.bokjipang;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class fragment_privacy extends Fragment {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_privacy, container, false);
+        return view;
+    }
+}
