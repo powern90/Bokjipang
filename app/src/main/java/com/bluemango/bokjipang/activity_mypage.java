6f49cb632e70beb0dda2fd2077d2e8cdbcdc2cba
xjwl0305@gmail.com <yoon8863!>
2021-03-25 18:32:40 +0900

    2021.3.25 윤인석 나의 게시물 / 내 정보

--- app/src/main/java/com/bluemango/bokjipang/activity_mypage.java
+++ app/src/main/java/com/bluemango/bokjipang/activity_mypage.java
@@ -3,12 +3,22 @@ package com.bluemango.bokjipang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class activity_mypage extends AppCompatActivity {
 
+    TextView back_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
+        back_btn = findViewById(R.id.mypage_back_btn);
+        back_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/bluemango/bokjipang/fragment_setting.java
+++ app/src/main/java/com/bluemango/bokjipang/fragment_setting.java
@@ -1,8 +1,12 @@
 package com.bluemango.bokjipang;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -30,5 +34,26 @@ public class fragment_setting extends Fragment {
             }
         });
         return view;
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        // destroy all menu and re-call onCreateOptionsMenu
+        getActivity().invalidateOptionsMenu();
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {    // 메뉴에 '내 게시물'페이지 상단바 만든뒤에 넣어둠 (검색 버튼)
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.main_top_navigation, menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.mypage_btn :
+                Intent intent = new Intent(getActivity(), activity_mypage.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
