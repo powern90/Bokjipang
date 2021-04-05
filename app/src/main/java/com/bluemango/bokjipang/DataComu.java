89333c1466d94c967e2abfc6aaeb93674caf109c
Ryeoly <youddbb@naver.com>
2021-04-05 23:13:51 +0900

    21.04.05 error, comunity(yun deuk ryeol)

--- app/src/main/java/com/bluemango/bokjipang/DataComu.java
+++ app/src/main/java/com/bluemango/bokjipang/DataComu.java
@@ -5,15 +5,20 @@ public class DataComu {
     private String content;
     private String  good_num;
     private String reply_num;
+    private String id;
+    private String datetime;
 
+    public String getDatetime(){ return datetime;}
+    public String getId(){return id;}
     public String getTitle(){return title;}
     public String getContent(){return content;}
     public String getGood_num(){return good_num;}
     public String getReply_num(){return reply_num;}
 
+    public void setDatetime(String datetime){this.datetime = datetime;}
     public void setTitle(String title){this.title = title;}
     public void setContent(String content){this.content = content;}
     public void setGood_num(String good_num){this.good_num = good_num;}
     public void setReply_num(String reply_num){this.reply_num = reply_num;}
-
+    public void setId(String id){this.id = id;}
 }
