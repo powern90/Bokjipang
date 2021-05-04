2a680fe7058963310af2dbc1e9cb743ea4c0bd05
ryeoly <youddbb@naver.com>
2021-05-04 21:56:35 +0900

    21.05.04 (yun deuk ryeol) 수정중

--- app/src/main/java/com/bluemango/bokjipang/mypost_listview_adapter.java
+++ app/src/main/java/com/bluemango/bokjipang/mypost_listview_adapter.java
@@ -1,103 +1,97 @@
 package com.bluemango.bokjipang;
 
-import android.content.ClipData;
 import android.content.Context;
-import android.util.Log;
+import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.ArrayList;
 
-//public class Post_RecyclerAdapter extends RecyclerView.Adapter<Post_RecyclerAdapter.ItemViewHolder>{
-//
-//    @NonNull
-//    @Override
-//    public Post_RecyclerAdapter.ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//        return null;
-//    }
-//
-//    @Override
-//    public void onBindViewHolder(@NonNull Post_RecyclerAdapter.ItemViewHolder holder, int position) {
-//
-//    }
-//
-//    @Override
-//    public int getItemCount() {
-//        return 0;
-//    }
-//}
-//class ItemViewHolder extends RecyclerView.ViewHolder{
-//    private TextView title;
-//    private TextView content;
-//    private TextView like;
-//
-//    ItemViewHolder(View itemView){
-//        super(itemView);
-//        title = itemView.findViewById(R.id.post_title);
-//        content = itemView.findViewById(R.id.post_content);
-//        like = itemView.findViewById(R.id.post_like);
-//    }
-//    void onBind(mypost_listview_item data){
-//        title.setText(data.get_title());
-//        content.setText(data.get_content());
-//        like.setText(data.get_like());
-//    }
-//}
-public class mypost_listview_adapter extends BaseAdapter {
-    ArrayList<mypost_listview_item> Post_list;
-    private static final String TAG = "ListView";
+public class mypost_listview_adapter extends RecyclerView.Adapter<mypost_listview_adapter.ViewHolder>{
+    private ArrayList<mypost_listview_item> mData = null ;
+    private Context context = null;
 
-    public mypost_listview_adapter(ArrayList<mypost_listview_item> datalist){
-        if(datalist == null){
-            Post_list = new ArrayList<mypost_listview_item>();
-        }
-        else{
-            Post_list = datalist;
+    // 아이템 뷰를 저장하는 뷰홀더 클래스.
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title ;
+        TextView content;
+        TextView comment;
+        TextView like;
+
+        ViewHolder(View itemView) {
+            super(itemView) ;
+
+            // 뷰 객체에 대한 참조. (hold strong reference)
+            title = itemView.findViewById(R.id.post_title) ;
+            content = itemView.findViewById(R.id.post_content);
+            like = itemView.findViewById(R.id.post_like);
+            comment = itemView.findViewById(R.id.post_comment);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = getAdapterPosition();
+                    if(pos != RecyclerView.NO_POSITION){
+                        mOnPopupClick(v, pos);
+                    }
+                }
+            });
         }
     }
-    @Override
-    public int getCount() { return Post_list.size(); }
-    @Override
-    public Object getItem(int position) {
-        return Post_list.get(position);
+
+    // 생성자에서 데이터 리스트 객체를 전달받음.
+    public mypost_listview_adapter(Context context, ArrayList<mypost_listview_item> list) {
+        this.context = context;
+        mData = list ;
     }
-    @Override
-    public long getItemId(int position) {
-        return position;
+
+    public void mOnPopupClick(View v, int pos) {
+        ArrayList<mypost_listview_item> list = this.mData;
+        Intent intent = new Intent(context, activity_community_post.class);
+
+        mypost_listview_item dataitem = list.get(pos);
+        intent.putExtra("data", dataitem.getIdx());
+        context.startActivity(intent);
     }
+
+
+    // onCreateViewHolder() - 아이템 뷰를 위한 뷰홀더 객체 생성하여 리턴.
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        final Context context = parent.getContext();
-        if(convertView == null){
-            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.sub_mypost, parent, false);
+    public mypost_listview_adapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext() ;
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) ;
+
+        View view = inflater.inflate(R.layout.sub_mypost, parent, false) ;
+        mypost_listview_adapter.ViewHolder vh = new mypost_listview_adapter.ViewHolder(view) ;
+
+        return vh ;
+    }
+
+
+    // onBindViewHolder() - position에 해당하는 데이터를 뷰홀더의 아이템뷰에 표시.
+    @Override
+    public void onBindViewHolder(mypost_listview_adapter.ViewHolder holder, int position) {
+        mypost_listview_item text = mData.get(position) ;
+        holder.title.setText(text.get_title());
+        holder.content.setText(text.get_content());
+        holder.like.setText(text.get_like());
+        holder.comment.setText(text.get_comment());
+    }
+
+
+    // getItemCount() - 전체 데이터 갯수 리턴.
+    @Override
+    public int getItemCount() {
+        if(mData == null) {
+            return 0;
         }
-
-        TextView title = convertView.findViewById(R.id.post_title);
-        TextView content = convertView.findViewById(R.id.post_content);
-        TextView like = convertView.findViewById(R.id.post_like);
-        TextView comment = convertView.findViewById(R.id.post_comment);
-
-        mypost_listview_item data = Post_list.get(position);
-        title.setText(data.get_title());
-        content.setText(data.get_content());
-        like.setText(data.get_like());
-        comment.setText(data.get_comment());
-        return convertView;
+        else return mData.size() ;
     }
-    public void addItem(String title, String content, String like, String comment){
-        mypost_listview_item data = new mypost_listview_item();
 
-        data.set_title(title);
-        data.set_content(content);
-        data.set_like(like);
-        data.set_comment(comment);
-        Post_list.add(data);
-    }
+
 }
